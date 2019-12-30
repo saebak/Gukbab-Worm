e8dbfdbede27f99883ded4dadf9a5c1530c14318
ProDesk <ProDesk@DESKTOP-6BJ6A54>
2019-12-30 16:24:59 +0900

    백업

---
+++ Saem's Gukbab/WebContent/js/result.js
@@ -0,0 +1,15 @@
+var obj = {
+    a: 1, 
+    b: 2, 
+    c: 3
+};
+
+for (var prop in obj) {
+    console.log(prop, obj[prop]); // a 1, b 2, c 3
+}
+
+document.getElementById("wasteInfo").submit(function(){
+	
+	sdf
+	
+});
